c02972ccb15f467b420127c2bdb4e5cc05f65c7c
Xenxa <saipeah1212@gmail.com>
2024-03-26 19:23:29 +0700

    Fix typo/styling

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -8,7 +8,7 @@ export default function ManageReservations(){
     return(
         <main className="w-[100%] flex flex-col items-center space-y-4 pb-[200px] pt-[15px]">
             <div className="text-2xl font-semibold text-black py-[15px]">Manage Your Booking</div>
-            <div className="relative min-w-[750px] w-[50vw] h-[60vh] space-y-2 shadow-2xl rounded-lg bg-white p-[45px]">
+            <div className="relative min-w-[750px] w-[50vw] h-[60vh] space-y-2 shadow-2xl rounded-3xl bg-white p-[45px]">
                 <div className='w-[50%] h-[55%] relative rounded-t-lg'>
                     <div className="relative text-black text-xl font-semibold p-[5px] pb-[15px]">Your Booking</div>
                     <img src='/img/dentist-default.jpg' alt='Dentist Image Template' className="w-100 h-[285px] rounded-xl" />
@@ -18,13 +18,13 @@ export default function ManageReservations(){
                         {/*dentistName*/}DentistName
                     </div>
                     <div className="text-black text-lg relative left-[30px] pb-[8px]">
-                        {/*dentistName*/}Experience : DentistExperience
+                        {/*dentistExp*/}Experience : DentistExperience
                     </div>
                     <div className="text-black text-lg relative left-[30px] pb-[32px]">
-                        {/*dentistName*/}Expertise : DentistExpertise
+                        {/*dentistExpertise*/}Expertise : DentistExpertise
                     </div>
                     <div className="text-black text-lg relative left-[30px]">
-                        {/*dentistName*/}AppointmentDate
+                        {/*ApptDate*/}AppointmentDate
                     </div>
                 </div>
                 <div className="absolute h-2/5 left-[60%] bottom-[-25%]">
