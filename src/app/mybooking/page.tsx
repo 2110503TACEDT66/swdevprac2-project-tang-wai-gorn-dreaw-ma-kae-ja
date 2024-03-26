5c562c0da406c90dd2b0a3fbecc25443ee88cc0b
Banknn <bank.tr2004@gmail.com>
2024-03-26 20:45:03 +0700

    add getBooking

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -1,37 +1,22 @@
-'use client'
-import { useSearchParams } from "next/navigation";
+import getDentists from "@/libs/getDentists";
+import DentistCatalog from "@/components/DentistCatalog";
+import { Suspense } from "react";
+import LinearProgress from "@mui/material/LinearProgress";
+import getBooking from "@/libs/getBooking";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import ReservationCart from "@/components/ReservationCart";
+export default async function MyBooking(){
 
-export default function ManageReservations(){
+    const session = await getServerSession(authOptions);
+    const bookings = getBooking(session?.user.token);
 
-    const urlParams = useSearchParams();
-
-    return(
-        <main className="w-[100%] flex flex-col items-center space-y-4 pb-[200px] pt-[15px]">
-            <div className="text-2xl font-semibold text-black py-[15px]">Manage Your Booking</div>
-            <div className="relative min-w-[750px] w-[50vw] h-[60vh] space-y-2 shadow-2xl rounded-3xl bg-white p-[45px]">
-                <div className='w-[50%] h-[55%] relative rounded-t-lg'>
-                    <div className="relative text-black text-xl font-semibold p-[5px] pb-[15px]">Your Booking</div>
-                    <img src='/img/dentist-default.jpg' alt='Dentist Image Template' className="w-100 h-[285px] rounded-xl" />
-                </div>
-                <div className="relative h-3/5 w-[50%] top-[-43%] left-[37%]"> 
-                    <div className="text-black text-lg relative left-[30px] pb-[8px]">
-                        {/*dentistName*/}DentistName
-                    </div>
-                    <div className="text-black text-lg relative left-[30px] pb-[8px]">
-                        {/*dentistExp*/}Experience : DentistExperience
-                    </div>
-                    <div className="text-black text-lg relative left-[30px] pb-[32px]">
-                        {/*dentistExpertise*/}Expertise : DentistExpertise
-                    </div>
-                    <div className="text-black text-lg relative left-[30px]">
-                        {/*ApptDate*/}AppointmentDate
-                    </div>
-                </div>
-                <div className="absolute h-2/5 left-[60%] bottom-[-25%]">
-                    <button className="bg-orange-500 rounded-lg p-[10px] mx-[10px]">Edit Booking</button>
-                    <button className="bg-red-600 rounded-lg p-[10px] mx-[10px]">Delete Booking</button>
-                </div>
-            </div>
+    return (
+        <main className="text-center p-5">
+            <h1 className="text-xl font-medium text-black">Manage Your Booking</h1>
+            <Suspense fallback={<p className="text-black">Loading ... <LinearProgress/></p>}>
+                <ReservationCart bookingJson={bookings}/>
+            </Suspense>
         </main>
     );
 }
--- src/components/ReservationCart.tsx
+++ src/components/ReservationCart.tsx
@@ -1,26 +1,48 @@
-import { removeReservation } from "@/redux/features/cartSlice"
-import { useAppSelector, AppDispatch } from "@/redux/store"
-import { useDispatch } from "react-redux"
-import { BookingJson } from "../../interfaces";
-import deleteBooking from "@/libs/deleteBooking";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Link } from "@mui/material"
+import ProductCard from "./ProductCard"
+import { BookingJson, Dentist, DentistJson, ReservationItem } from "../../interfaces"
+import updateBooking from "@/libs/updateBooking"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import deleteBooking from "@/libs/deleteBooking"
 
-export default async function ReservationCart({bookingJson} : {bookingJson:BookingJson}){
-    const bookingJsonReady = await bookingJson;
+export default async function ReservationCart({bookingJson}: {bookingJson:BookingJson}){
+    
+    const bookingJsonReady = await bookingJson
     const session = await getServerSession(authOptions);
+    
+
     return(
-        <>
-        {
-            bookingJsonReady.data.map((reservationItem) => (
-                <div className="bg-slate-200 rounded px-5  mx-5 py-2 my-2">
-                    <div className="text-xl">{reservationItem.user}</div>
-                    <div className="text-sm">Appointment Date: {reservationItem.apptDate}</div>
-                    <div className="text-sm">Dentist: {reservationItem.dentist}</div>
-                    <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm" onClick={() => deleteBooking(reservationItem.apptDate, reservationItem.user, reservationItem.dentist, reservationItem.id, session?.user.token)}>Remove from Cart</button>
-                </div>
-            ))
-        }
-        </>
+        <div>
+            <div style={{margin: "20px", display: "flex", flexDirection:"row", alignContent:"space-around", justifyContent:"space-around", flexWrap:"wrap"}}>
+                {
+                    bookingJsonReady.data.map((booking:ReservationItem)=> (
+                        <div className="relative min-w-[750px] w-[50vw] h-[80vh] space-y-2 shadow-2xl rounded-3xl bg-white p-[45px]">
+                            <div className='w-[50%] h-[55%] relative rounded-t-lg'>
+                                <img src='/img/dentist-default.jpg' alt='Dentist Image Template' className="w-100 h-[285px] rounded-xl" />
+                            </div>
+                            <div className="relative h-3/5 w-[50%] top-[-43%] left-[37%]"> 
+                                <div className="text-black text-lg relative left-[30px] pb-[8px]">
+                                    {/* dentistName*/}DentistName : {booking.dentist.name}
+                                </div>
+                                <div className="text-black text-lg relative left-[30px] pb-[8px]">
+                                    {/*dentistExp*/}Experience : {booking.dentist.experience}
+                                </div>
+                                <div className="text-black text-lg relative left-[30px] pb-[32px]">
+                                    {/*dentistExpertise*/}Expertise : {booking.dentist.expertise}
+                                </div>
+                                <div className="text-black text-md relative left-[30px]">
+                                    {/* ApptDate*/}AppointmentDate : {booking.apptDate}
+                                </div>
+                            </div>
+                            <div className="absolute h-1/5 left-[60%] bottom-[5%] flex flex-row">
+                                <button className="bg-orange-500 rounded-lg p-[10px] mx-[10px]">Edit Booking</button>
+                                <button className="bg-red-600 rounded-lg p-[10px] mx-[10px]" onClick={deleteBooking(booking.apptDate, booking.user, booking.dentist, booking.id, session?.user.token as string)}>Delete Booking</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
