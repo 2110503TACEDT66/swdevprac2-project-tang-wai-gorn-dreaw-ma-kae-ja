1fdecb430c0c4327636708a7ba15747a6d44c467
Banknn <bank.tr2004@gmail.com>
2024-03-31 13:09:11 +0700

    fix booking/page, addBooking

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,40 +1,38 @@
 'use client'
-import DateReserve from "@/components/DateReserve";
-import dayjs, { Dayjs } from "dayjs";
-import { useSearchParams } from "next/navigation";
-import { useState } from "react";
-import { TextField } from "@mui/material";
-import addBooking from "@/libs/addBooking";
-export default function Reservations(){
-
-    const urlParams = useSearchParams();
-    const did = urlParams.get('id');
-    const dentistName = urlParams.get('name');
+import DateReserve from "@/components/DateReserve"
+import dayjs, { Dayjs } from "dayjs"
+import { useSearchParams } from "next/navigation"
+import { useState } from "react"
+import { TextField } from "@mui/material"
+import addBooking from "@/libs/addBooking"
+export default function Reservations() {
+    const urlParams = useSearchParams()
+    const did = urlParams.get('id')
+    const dentistName = urlParams.get('name')
     const userid = urlParams.get('userid')
     const token = urlParams.get('token')
-    
+
     const makeReservation = () => {
         if(did && pickupDate && userid) {
-            addBooking(dayjs(pickupDate).format("YYYY/MM/DD"), userid, did, token)
+            addBooking(dayjs(pickupDate).format('YYYY/MM/DD'), userid, did, token)
         }
     }
 
-    const [name, setName] = useState<string | null>(dentistName);
-    const [pickupDate, setPickupDate] = useState<Dayjs|null>(null)
+    const [name, setName] = useState<string | null>(dentistName)
+    const [pickupDate, setPickupDate] = useState<Dayjs | null>(null)
 
-    return(
+    return (
         <main className="w-[100%] flex flex-col items-center space-y-4">
-            <div className="text-xl font-medium">New Dentist Booking</div>
+            <div className="texl-xl font-medium">New Dentist Booking</div>
             <div className="w-fit space-y-2">
                 <TextField variant="standard" name="Name" label="Name" defaultValue={dentistName}
-                onChange={(e)=>{setName(e.target.value)}}/> 
+                onChange={(e)=>{setName(e.target.value)}}/>
             </div>
             <div className="w-fit space-y-2">
                 <div className="text-md text-left text-gray-600">Pick-Up Appointment Date</div>
                 <DateReserve onDateChange={(value:Dayjs) => {setPickupDate(value)}}/>
             </div>
-
             <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm" onClick={makeReservation}>Book Dentist</button>
         </main>
-    );
+    )
 }
--- src/libs/addBooking.tsx
+++ src/libs/addBooking.tsx
@@ -1,20 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-export default async function addBooking(apptDate: string, user: string, did: string, token:string | null) {
+export default async function addBooking(apptDate: string, user: string, did: string, token: string | null) {
     const userId = new mongoose.Types.ObjectId(user);
-    
+
     const response = await fetch(`${process.env.BACKEND_URL}/api/v1/dentists/${did}/bookings`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "application.json",
             authorization: `Bearer ${token}`
         },
         body: JSON.stringify({
             apptDate: apptDate,
             user: user
-        }),
+        })
     });
-    if(response.status === 400) alert('you can only book 1 booking!!')
-    else if (response.status === 401) throw new Error("Failed to add a booking");
+    if(response.status === 400) alert('you can only book 1 booking!!');
+    else if(response.status === 401) throw new Error("Failed to add a booking");
     return await response.json();
 }
