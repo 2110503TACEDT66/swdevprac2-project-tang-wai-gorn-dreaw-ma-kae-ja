e8d0c38dce1879a27acbcce8ca0a85f14aea5a8e
Banknn <bank.tr2004@gmail.com>
2024-03-31 20:06:16 +0700

    fix getDentists

--- src/libs/getDentists.tsx
+++ src/libs/getDentists.tsx
@@ -1,6 +1,6 @@
 export default async function getDentists(){
 
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/dentists`, {next: {tags:['dentists']}})
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/dentists`)
     if(!response.ok) throw new Error("Failed to fetch dentists")
     return await response.json()
 }
